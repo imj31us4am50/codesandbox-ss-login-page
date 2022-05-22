b5158f8ad2886515520cab0e33404a66ce547fa7
Volosenco Andreea-Laura <91089388+imj31us4am50@users.noreply.github.com>
2022-05-22 09:03:03 +0300

    upd

--- desccomponents/Title.js
+++ desccomponents/Title.js
@@ -4,7 +4,7 @@ export default function Title() {
   return (
     <div
       className="container-fluid h1 text-center mx-auto title-of-desc"
-      style="TitleCSS"
+      style={TitleCSS}
     >
       Pentru o experiență absolută și o modalitate mai bună de a învăța
       eficient!
