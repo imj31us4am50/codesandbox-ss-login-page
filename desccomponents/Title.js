5e2c3356e2636afa94a9d9af265a663233e72e01
Volosenco Andreea-Laura <91089388+imj31us4am50@users.noreply.github.com>
2022-05-22 09:00:18 +0300

    upd

--- desccomponents/Title.js
+++ desccomponents/Title.js
@@ -2,7 +2,10 @@ import TitleCSS from "/desccomponents/Title.css";
 
 export default function Title() {
   return (
-    <div className="container-fluid h1 text-center mx-auto title-of-desc">
+    <div
+      className="container-fluid h1 text-center mx-auto title-of-desc"
+      style="TitleCSS"
+    >
       Pentru o experiență absolută și o modalitate mai bună de a învăța
       eficient!
     </div>
